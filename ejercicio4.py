65664e21fc9cc041d6fd7bdf01cd4089c4c780a0
Kevin Martinez <kevin.illu@galileo.edu>
2026-03-18 01:45:58 +0000

    inicio ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,22 @@
+# Ejercicio #4: (Nombre del archivo: ejercicio4.py)
+# Escriba un programa que lea del usuario un password,
+# hasta que cumpla con las condiciones necesarias para poder ser utilizado.
+# Si el password no cumple con las condiciones, debe desplegar "Weak password"
+# y pedir que se ingrese otro password. Si el password cumple con la condición,
+# debe terminar el programa y desplegar "Strong password".
+# Las condiciones que debe cumplir el password son:
+
+# 			(1) Tener por lo menos 8 caracteres
+# 			(2) Contenga mas de una vocal
+# 			(3) Contenga mas de un digito
+
+valid_lenght = False
+has_vowels = False
+has_digits = False
+
+message = f"""
+  (1) Tener por lo menos 8 caracteres [{valid_lenght}]
+  (2) Contenga mas de una vocal       [{has_vowels}]
+  (3) Contenga mas de un digito       [{has_digits}]
+"""
+print(message)
