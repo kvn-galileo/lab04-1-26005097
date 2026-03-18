c3cf961742b16a4c6625bc7fc5e6d09589496f4a
Kevin Martinez <kevin.illu@galileo.edu>
2026-03-18 06:15:03 +0000

    ejercicio4

--- ejercicio4.py
+++ ejercicio4.py
@@ -10,20 +10,75 @@
 # 			(2) Contenga mas de una vocal
 # 			(3) Contenga mas de un digito
 
-valid_lenght = False
+first_attempt = True
+valid_length = False
 has_vowels = False
 has_digits = False
 
-validations = not valid_lenght and not has_vowels and not has_digits
+while True:
+    mark_lenght = "x" if valid_length else " "
+    mark_vowels = "x" if has_vowels else " "
+    mark_numbers = "x" if has_digits else " "
 
-while validations:
-    veryfication_message = f"""
-      (1) Tener por lo menos 8 caracteres [{valid_lenght}]
-      (2) Contenga mas de una vocal       [{has_vowels}]
-      (3) Contenga mas de un digito       [{has_digits}]
-    """    
-    print("Type a string password: ")
+    verification_message = f"""
+      (1) Tener por lo menos 8 caracteres [{mark_lenght}]
+      (2) Contenga mas de una vocal       [{mark_vowels}]
+      (3) Contenga mas de un digito       [{mark_numbers}]
+    """
+
+    print(verification_message)
+
+    if not first_attempt:
+      print('Weak password \n')
+
+    password = input("Password: ")
+
+    valid_length = len(password) >= 8
+
+    str_count = 0
+    for letter in password:
+      lower_vowel = letter.lower()
+      
+      if lower_vowel == 'a' or lower_vowel == 'e' or lower_vowel == 'i' or lower_vowel == 'o' or lower_vowel == 'u':
+        str_count += 1
+
+      if (str_count > 1):
+        has_vowels = True
+        break
+    
+    if str_count <= 1:
+      has_vowels = False
 
 
+    digit_count = 0
+    for letter in password:
+      if letter.isdigit():
+        digit_count += 1
+      
+      if digit_count > 1:
+        has_digits = True
+        break
+    
+    if digit_count <= 1:
+      has_digits = False
 
+    invalid_password = password
+    validations = valid_length and has_vowels and has_digits
+
+    if validations:
+      break
+    else:
+      first_attempt = False
+
+mark_lenght = "x" if valid_length else " "
+mark_vowels = "x" if has_vowels else " "
+mark_numbers = "x" if has_digits else " "
+verification_message = f"""
+      Tu password debe:
+      (1) Tener por lo menos 8 caracteres [{mark_lenght}]
+      (2) Contenga mas de una vocal       [{mark_vowels}]
+      (3) Contenga mas de un digito       [{mark_numbers}]
+    """
+print(verification_message)
+message = 'Strong password.'
 print(message)
